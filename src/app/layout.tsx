07feea959ef85c645805932acbf6a08291a992d4
Martyna Bielenda <martynabielenda@gmail.com>
2024-12-09 18:24:05 +0100

    Added sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
+/* eslint-disable prettier/prettier */
 import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
+
+import { AppSidebar } from '@/components/AppSidebar/app-sidebar';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 
 export const metadata: Metadata = {
   title: 'Share Notes',
@@ -14,7 +18,15 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          <SidebarProvider>
+            <AppSidebar />
+            <SidebarTrigger />
+            <main>
+              {children}
+            </main>
+          </SidebarProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
